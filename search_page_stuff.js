56993447dc448ffb1144fcd3ff31b5ca494ccf4b
Cyrus <cbilpodi@umd.edu>
2024-04-16 20:37:49 -0400

    Added a partial search page

---
+++ search_page_stuff.js
@@ -0,0 +1,9 @@
+import performSearch  from 'HomePage.html'
+
+function showResults() {
+    var searchItemPane = document.getElementById("searchResults");
+    // Load results from the Home Page's JavaScript.
+    var listItem = document.createElement("ul");
+
+
+}
